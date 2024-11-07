faf63ea72e484fb225114f3a5e0285e1128850f4
Paulinagarciag <102259331+Paulinagarciag@users.noreply.github.com>
2024-11-07 00:46:54 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/MaquinaAnestesia/',  // Reemplaza 'tu-repo' con el nombre de tu repositorio
   plugins: [react()],
-})
+});
